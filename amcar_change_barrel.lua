24573853534b8a51a7b7671aa31226e8eb4d6904
Alcat101 <adhanika15@gmail.com>
2015-12-21 06:15:04 +0700

    Initial state

---
+++ amcar_change_barrel.lua
@@ -0,0 +1,8 @@
+if not AMCARbarrel then
+
+	tweak_data.weapon.factory.wpn_fps_ass_amcar.override = {
+		wpn_fps_m4_uupg_b_medium_vanilla = {unit = "units/payday2/weapons/wpn_fps_ass_m4_pts/wpn_fps_m4_uupg_b_long", third_unit = "units/payday2/weapons/wpn_third_ass_m4_pts/wpn_third_m4_uupg_b_long"}
+	}
+
+AMCARbarrel = true
+end
